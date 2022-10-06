4a3a77c8d1447e39c06c9fe24c169213986cac6a
sunhwa park <sunhwapark@sunhwaui-MacBookPro.local>
2022-10-06 15:23:11 +0900

    test

---
+++ A_SH/tmp.js
@@ -0,0 +1,15 @@
+// 1. ?
+
+// 2. fs 사용
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+// string array
+let lineInput = fs.readFileSync(filePath).toString().trim().split("\n");
+let wordInput = fs.readFileSync(filePath).toString().trim().split(/\s/g);
+
+const input = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+
+console.log(lineInput);
+console.log(wordInput);
